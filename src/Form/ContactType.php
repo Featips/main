ec4eafea46885a02f63fdd963a0a529668fb49a9
DevKco <ken.condom@outlook.fr>
2022-01-07 17:16:07 +0100

    ajout d'article, contact, user

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -9,23 +9,57 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactType extends AbstractType
 {
+
+    // private $entityManager;
+
+    // public function __construct(EntityManagerInterface $entityManager)
+    // {
+    //     $this->entityManager = $entityManager;
+    // }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    {   
+        /* @var User $user*/
+
+        // $user=$this->getUser();
+        // var_dump($user);
+        // $firstname= ""; 
+        // $lastname= "";
+        // $email="";
+
+        // if($user->getUser()){
+            // $firstname = $user.firstname ;
+        // }
+        
+        
         $builder
         ->add('firstname', TextType::class,[
-            'label' => 'prénom',
-        ])
+                    'label' => 'prénom',
+                    // 'value' => $firstname,
+
+                ])
         ->add('lastname', TextType::class,[
-            'label' => 'nom',
-        ])
+                    'label' => 'nom',
+                    // 'value' => $lastname,
+               ])
         ->add('email', EmailType::class, [
-            'disabled' => true,
-            'label' => 'mail',
-        ])
+                    'disabled' => true,
+                    'label' => 'mail',
+                ])  
         ->add('texte', TextareaType::class,[
             'label' => 'Votre message'
         ])
