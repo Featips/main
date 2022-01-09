c7e9cae1026cff17a28b94144304af33587ee314
JuliePoteau <94682445+JuliePoteau@users.noreply.github.com>
2022-01-09 17:49:05 +0100

    starting dev front

---
+++ public/assets/featips/pageArticles/pageArticle.html.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/pageArticle.css" type="text/css">
+  <link rel="stylesheet" href="../splide-3.6.9/dist/css/splide.min.css">
+    <title>pageArticle</title>
+  </head>
+  <body>
+    <?php require_once 'header.html.php'; ?>
+  
+    <main>
+    <div class="banniere-artile">
+    <div class="banniere-article-img"></div>
+    <p>Bienvenue sur <span id="brand1">FeaTips.fr</span>. <br>Apprenez à devenir votre propre coach sportif !</p>
+  </div>
+  <main>
+    <section class="carousel">
+      <h2>Articles</h2>
+      <div class="splide">
+        <div class="splide__track">
+          <ul class="splide__list">
+            <li class="splide__slide"><a href="#"><img src="assets/imgFeaTips/BeachVolley.PNG" alt="beach-volley" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, quidem nihil praesentium vero tenetur, magni numquam eaque magnam doloremque repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+            <li class="splide__slide"><a href="#"><img src="assets/imgFeaTips/juliacha.JPG" alt="Climb moutain" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, magni numquam eaque magnam doloremque repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+            <li class="splide__slide"><a href="#">
+                <img src="assets/imgFeaTips/basketball-court-g55e624258_1920.jpg" alt="basketball-court" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, quidem nihil praesentium vero tenetur, magni numquam eaque magnam doloremque repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+            <li class="splide__slide"><a href="#">
+                <img src="assets/imgFeaTips/basketball-court-g55e624258_1920.jpg" alt="basketball-court" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+              <li class="splide__slide"><a href="#"><img src="assets/imgFeaTips/BeachVolley.PNG" alt="beach-volley" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, quidem nihil praesentium vero tenetur, magni numquam eaque magnam doloremque repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+            <li class="splide__slide"><a href="#"><img src="assets/imgFeaTips/juliacha.JPG" alt="Climb moutain" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, magni numquam eaque magnam doloremque repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+
+
+              
+            <li class="splide__slide"><a href="#">
+                <img src="assets/imgFeaTips/basketball-court-g55e624258_1920.jpg" alt="basketball-court" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, quidem nihil praesentium vero tenetur, magni numquam eaque magnam doloremque repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+            <li class="splide__slide"><a href="#">
+                <img src="assets/imgFeaTips/basketball-court-g55e624258_1920.jpg" alt="basketball-court" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+              <li class="splide__slide"><a href="#"><img src="assets/imgFeaTips/BeachVolley.PNG" alt="beach-volley" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, quidem nihil praesentium vero tenetur, magni numquam eaque magnam doloremque repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+            <li class="splide__slide"><a href="#"><img src="assets/imgFeaTips/juliacha.JPG" alt="Climb moutain" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, magni numquam eaque magnam doloremque repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+            <li class="splide__slide"><a href="#">
+                <img src="assets/imgFeaTips/basketball-court-g55e624258_1920.jpg" alt="basketball-court" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, quidem nihil praesentium vero tenetur, magni numquam eaque magnam doloremque repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+            <li class="splide__slide"><a href="#">
+                <img src="assets/imgFeaTips/basketball-court-g55e624258_1920.jpg" alt="basketball-court" />
+                <div class="description-article">
+                  <p class="title-article">Titre de l'article</p>
+                  <p class="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint consequatur aperiam? Libero est commodi autem voluptate, repellat consectetur iste.</p>
+                  <p class="authors">Auteurs</p>
+                </div>
+              </a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+      <!-- <img src="../imgFeaTips/img/imgArticle.jpg" alt="Image Header article" />
+      <div class="filter">
+        <p>Filtrer la recherche :</p>
+        <br />
+        <ul>
+          <li>Par Coach</li>
+          <li>Par Sport</li>
+        </ul>
+      </div>
+      <section class="par-coach">
+        <a href="#"
+          ><img src="../imgFeaTips/img/BeachVolley.PNG" alt="beach-volley" />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p>
+        </a>
+        <a href="#"
+          ><img src="../imgFeaTips/img/BeachVolley.PNG" alt="beach-volley" />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p>
+        </a>
+        <a href="#"
+          ><img src="../imgFeaTips/img/BeachVolley.PNG" alt="beach-volley" />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p>
+        </a>
+        <a href="#"
+          ><img src="../imgFeaTips/img/BeachVolley.PNG" alt="beach-volley" />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p>
+        </a>
+        <a href="#"
+          ><img src="../imgFeaTips/img/BeachVolley.PNG" alt="beach-volley" />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p>
+        </a>
+        <a href="#"
+          ><img src="../imgFeaTips/img/BeachVolley.PNG" alt="beach-volley" />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p>
+        </a>
+      </section>
+      <section class="par-sport">
+        <a href="#"
+          ><img
+            src="../imgFeaTips/img/basketball-court-g55e624258_1920.jpg"
+            alt="basketball-court"
+          />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p></a
+        >
+        <a href="#"
+          ><img
+            src="../imgFeaTips/img/basketball-court-g55e624258_1920.jpg"
+            alt="basketball-court"
+          />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p></a
+        >
+        <a href="#"><img
+            src="../imgFeaTips/img/basketball-court-g55e624258_1920.jpg"
+            alt="basketball-court"
+          />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p></a
+        >
+        <a href="#"
+          ><img
+            src="../imgFeaTips/img/basketball-court-g55e624258_1920.jpg"
+            alt="basketball-court"
+          />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p></a
+        >
+        <a href="#"
+          ><img
+            src="../imgFeaTips/img/basketball-court-g55e624258_1920.jpg"
+            alt="basketball-court"
+          />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p></a
+        >
+        <a href="#"
+          ><img
+            src="../imgFeaTips/img/basketball-court-g55e624258_1920.jpg"
+            alt="basketball-court"
+          />
+          <p class="paragraph-article">Titre de l'article</p>
+          <p class="description-text">Texte descriptif</p>
+          <p class="authors">Auteurs</p></a>
+      </section>
+      </main> -->
+    
+      <section class="cta-forum">
+      <div class="forum-img"></div>
+      <a href="#">
+        <p>Rejoignez la communauté <span id="brand2">FeaTips</span> et entraidez-vous<br>dans notre Forum dédié !</p>
+      </a>
+    </section>
+    <?php require_once 'footer.html.php'; ?>
+</body>
+<script src="../splide-3.6.9/dist/js/splide.min.js"></script>
+<script src="./js/script-article.js"></script>
+
+</html>
