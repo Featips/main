c39dd52d83df60b8ca671521b79e1e814c894acb
JuliePoteau <94682445+JuliePoteau@users.noreply.github.com>
2022-01-10 12:38:17 +0100

    programmes page done, Articles page without responsive, migrations

--- migrations/Version20220110084005.php
+++ migrations/Version20220110084005.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220107095305 extends AbstractMigration
+final class Version20220110084005 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,38 +22,44 @@ final class Version20220107095305 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, content LONGTEXT NOT NULL, is_premium TINYINT(1) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE article_category (article_id INT NOT NULL, category_id INT NOT NULL, INDEX IDX_53A4EDAA7294869C (article_id), INDEX IDX_53A4EDAA12469DE2 (category_id), PRIMARY KEY(article_id, category_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article_program (article_id INT NOT NULL, program_id INT NOT NULL, INDEX IDX_7F3F33337294869C (article_id), INDEX IDX_7F3F33333EB8070A (program_id), PRIMARY KEY(article_id, program_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article_user (article_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_3DD151487294869C (article_id), INDEX IDX_3DD15148A76ED395 (user_id), PRIMARY KEY(article_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE article_comments (id INT AUTO_INCREMENT NOT NULL, id_user_id INT NOT NULL, id_article_id INT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL, content LONGTEXT NOT NULL, INDEX IDX_A76624179F37AE5 (id_user_id), INDEX IDX_A766241D71E064B (id_article_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE article_user (id INT AUTO_INCREMENT NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE article_user_article (article_user_id INT NOT NULL, article_id INT NOT NULL, INDEX IDX_897B67FD4ABC9722 (article_user_id), INDEX IDX_897B67FD7294869C (article_id), PRIMARY KEY(article_user_id, article_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE article_user_user (article_user_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_2D27CAA44ABC9722 (article_user_id), INDEX IDX_2D27CAA4A76ED395 (user_id), PRIMARY KEY(article_user_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE media_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE program (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, is_premium TINYINT(1) NOT NULL, profile_pic VARCHAR(255) DEFAULT NULL, email VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE article_category ADD CONSTRAINT FK_53A4EDAA7294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE article_category ADD CONSTRAINT FK_53A4EDAA12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE article_program ADD CONSTRAINT FK_7F3F33337294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE article_program ADD CONSTRAINT FK_7F3F33333EB8070A FOREIGN KEY (program_id) REFERENCES program (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE article_user ADD CONSTRAINT FK_3DD151487294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE article_user ADD CONSTRAINT FK_3DD15148A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE article_comments ADD CONSTRAINT FK_A76624179F37AE5 FOREIGN KEY (id_user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE article_comments ADD CONSTRAINT FK_A766241D71E064B FOREIGN KEY (id_article_id) REFERENCES article (id)');
-        $this->addSql('ALTER TABLE article_user_article ADD CONSTRAINT FK_897B67FD4ABC9722 FOREIGN KEY (article_user_id) REFERENCES article_user (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE article_user_article ADD CONSTRAINT FK_897B67FD7294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE article_user_user ADD CONSTRAINT FK_2D27CAA44ABC9722 FOREIGN KEY (article_user_id) REFERENCES article_user (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE article_user_user ADD CONSTRAINT FK_2D27CAA4A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE article_category DROP FOREIGN KEY FK_53A4EDAA7294869C');
+        $this->addSql('ALTER TABLE article_program DROP FOREIGN KEY FK_7F3F33337294869C');
+        $this->addSql('ALTER TABLE article_user DROP FOREIGN KEY FK_3DD151487294869C');
         $this->addSql('ALTER TABLE article_comments DROP FOREIGN KEY FK_A766241D71E064B');
-        $this->addSql('ALTER TABLE article_user_article DROP FOREIGN KEY FK_897B67FD7294869C');
-        $this->addSql('ALTER TABLE article_user_article DROP FOREIGN KEY FK_897B67FD4ABC9722');
-        $this->addSql('ALTER TABLE article_user_user DROP FOREIGN KEY FK_2D27CAA44ABC9722');
         $this->addSql('ALTER TABLE article_category DROP FOREIGN KEY FK_53A4EDAA12469DE2');
+        $this->addSql('ALTER TABLE article_program DROP FOREIGN KEY FK_7F3F33333EB8070A');
+        $this->addSql('ALTER TABLE article_user DROP FOREIGN KEY FK_3DD15148A76ED395');
+        $this->addSql('ALTER TABLE article_comments DROP FOREIGN KEY FK_A76624179F37AE5');
         $this->addSql('DROP TABLE article');
         $this->addSql('DROP TABLE article_category');
-        $this->addSql('DROP TABLE article_comments');
+        $this->addSql('DROP TABLE article_program');
         $this->addSql('DROP TABLE article_user');
-        $this->addSql('DROP TABLE article_user_article');
-        $this->addSql('DROP TABLE article_user_user');
+        $this->addSql('DROP TABLE article_comments');
         $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE media_type');
         $this->addSql('DROP TABLE program');
+        $this->addSql('DROP TABLE role');
+        $this->addSql('DROP TABLE user');
     }
 }
